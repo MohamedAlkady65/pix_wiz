0504fb367ce3129ae7b0ef3f64c815a7d4ff5aa1
MohamedAlkady <mohamedalkady65@gmail.com>
2023-12-16 06:16:26 +0200

    step in home screen ui

--- lib/Features/Home/home_screen.dart
+++ lib/Features/Home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:pix_wiz/Core/colors.dart';
 import 'package:pix_wiz/Features/edit/logic/edit/edit_image_cubit.dart';
 import 'package:pix_wiz/Features/edit/presentation/edit/edit_screen.dart';
 
@@ -10,37 +12,50 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Image Processing"),
-      ),
-      body: Align(
-        alignment: Alignment.center,
+      backgroundColor: const Color(0xFFf6f6f6),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             const Spacer(
               flex: 1,
             ),
+            Align(
+              alignment: Alignment.center,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(99999),
+                  child: SvgPicture.asset(
+                    'assets/images/logo1.svg',
+                    // width: 150,
+                  )),
+            ),
+            const Spacer(
+              flex: 1,
+            ),
             const Text(
-              "Select Image To Process",
-              style: TextStyle(fontSize: 24),
+              "Choose Image",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
             ),
-            const Spacer(
-              flex: 6,
+            const SizedBox(
+              height: 25,
             ),
-            ElevatedButton(
-                onPressed: () async {
-                  await pickImage(context, source: ImageSource.camera);
-                },
-                child: const Text("From Camera")),
-            const Spacer(
-              flex: 2,
+            CustomButton(
+              text: 'Camera',
+              onPressed: () {
+                pickImage(context, source: ImageSource.camera);
+              },
             ),
-            ElevatedButton(
-                onPressed: () async {
-                  await pickImage(context, source: ImageSource.gallery);
-                },
-                child: const Text("From Gallery")),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButton(
+              text: 'Gallery',
+              onPressed: () {
+                pickImage(context, source: ImageSource.gallery);
+              },
+            ),
             const Spacer(
               flex: 1,
             ),
@@ -62,5 +77,26 @@ class HomeScreen extends StatelessWidget {
             builder: (context) => const EditScreen(),
           ));
     }
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.text, required this.onPressed});
+  final String text;
+  final void Function() onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: kPrimaryColor,
+            fixedSize: const Size.fromHeight(60),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16))),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 22, fontWeight: FontWeight.w500),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,25 @@ class PixWiz extends StatelessWidget {
     final EditImageCubit editImageCubit = EditImageCubit();
     return BlocProvider(
       create: (context) => editImageCubit,
-      child: MaterialApp(
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark(),
-        home: const HomeScreen(),
+        home: HomeScreen(),
       ),
     );
+  }
+
+  MaterialColor primaryColor() {
+    return const MaterialColor(0xFF2A93DA, {
+      50: Color.fromRGBO(42, 147, 218, .1),
+      100: Color.fromRGBO(42, 147, 218, .2),
+      200: Color.fromRGBO(42, 147, 218, .3),
+      300: Color.fromRGBO(42, 147, 218, .4),
+      400: Color.fromRGBO(42, 147, 218, .5),
+      500: Color.fromRGBO(42, 147, 218, .6),
+      600: Color.fromRGBO(42, 147, 218, .7),
+      700: Color.fromRGBO(42, 147, 218, .8),
+      800: Color.fromRGBO(42, 147, 218, .9),
+      900: Color.fromRGBO(42, 147, 218, 1),
+    });
   }
 }
