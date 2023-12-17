1f2b64f9b298566c90ee933519bc5391f22e7834
MohamedAlkady <mohamedalkady65@gmail.com>
2023-12-17 10:58:59 +0200

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pix_wiz/Features/edit/logic/edit/edit_image_cubit.dart';
 import 'package:pix_wiz/Features/home/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PixWiz());
 }
 
